21eed6fcd341f1e3889f2bbee064bb665810a5a4
orjan helstrom <orjan.helstrom@gmail.com>
2013-09-24 21:54:39 +0200

    Changed database to sqlite Added database file to gitignore

--- FoodDay/settings.py
+++ FoodDay/settings.py
@@ -11,13 +11,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'foodday',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'foodday_database_file',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'orjan',
+        'USER': '',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
