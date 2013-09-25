d41eb346414ac414f0da5e3ef521b61a7ea89b8a
orjan helstrom <orjan.helstrom@gmail.com>
2013-09-25 21:35:42 +0200

    Added mass and massunit Added pretty_price_per_mass that shows pricer per massunit in the view Updated ProductType view to only show ProductType name.

--- Products/models.py
+++ Products/models.py
@@ -3,9 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
+
+    KILOGRAM = 'Kilogram'
+    GRAM = 'gram'
+    LITER = 'liter'
+    DESILITER = 'desiliter'
+    MILLILITER = 'milliliter'
+    UNITS = ((KILOGRAM, 'kg'), (GRAM, 'g'), (LITER, 'l'), (DESILITER, 'dl'), (MILLILITER, 'ml'))
+
     #Fields
     name = models.CharField(max_length=150)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    mass = models.DecimalField(max_digits=10, decimal_places=2)
+    massUnit = models.CharField(max_length=150, choices=UNITS)
 
     #Relations
     producer = models.ForeignKey('Producer')
@@ -13,6 +23,27 @@ class Product(models.Model):
 
     def __unicode__(self):
         return self.name + ' kr.' + str(self.price)
+
+    def normalized_mass_unit(self):
+        """ Returns a tuple with (0.5,  KILOGRAM)"""
+        if(self.massUnit == self.GRAM):
+            return (self.mass/1000, self.KILOGRAM)
+        elif (self.massUnit == self.DESILITER):
+            return (self.mass/10, self.LITER)
+        elif (self.massUnit == self.MILLILITER):
+            return (self.mass/1000, self.LITER)
+        else:
+            return (self.mass, self.massUnit)
+
+    def price_per_mass(self):
+        """ """
+        mass, unit = self.normalized_mass_unit()
+        return (self.price/mass, unit)
+
+    def pretty_price_per_mass(self):
+        price, unit = self.price_per_mass()
+        return u'{price} kr/{unit}'.format(price=price, unit=unit)
+
 
 
 class Producer(models.Model):
