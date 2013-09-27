7b5b7546397aa2fc1161748ae4354d48e6e51e2d
orjan helstrom <orjan.helstrom@gmail.com>
2013-09-27 09:18:47 +0200

    Added a description TextField to product. Added a vegetarian and a vegan BooleanField to product. Updated view for both.

--- Products/models.py
+++ Products/models.py
@@ -13,9 +13,12 @@ class Product(models.Model):
 
     #Fields
     name = models.CharField(max_length=150)
+    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     mass = models.DecimalField(max_digits=10, decimal_places=2)
     massUnit = models.CharField(max_length=150, choices=UNITS)
+    vegetarian = models.BooleanField()
+    vegan = models.BooleanField()
 
     #Relations
     producer = models.ForeignKey('Producer')
