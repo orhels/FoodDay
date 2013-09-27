512d68f79d23a427b3eeb5721e64b8fbf14d0966
orjan helstrom <orjan.helstrom@gmail.com>
2013-09-27 10:09:26 +0200

    Added ProductType index and detail view. Added link from Product detailView to ProductType detailView, so one can see all products of the same kind.

--- FoodDay/urls.py
+++ FoodDay/urls.py
@@ -1,7 +1,7 @@
 from django import views
 from django.conf.urls import patterns, include, url
 
-from Products.views import ProductIndexView, ProductDetailView
+from Products.views import ProductDetailView, ProductIndexView, ProductTypeDetailView, ProductTypeIndexView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,6 +15,10 @@ urlpatterns = patterns('',
     # PRODUCT
     url(r'^product/$', ProductIndexView.as_view(), name='product_list'),
     url(r'^product/(?P<pk>\d+)/$', ProductDetailView.as_view(), name='product_detail'),
+
+    # PRODUCT_TYPE
+    url(r'^productType/$', ProductTypeIndexView.as_view(), name='product_type_list'),
+    url(r'^productType/(?P<pk>\d+)/$', ProductTypeDetailView.as_view(), name='product_type_detail'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
