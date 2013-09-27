4dfa5985acd131d4db53c8f4c79d58e2eb952443
orjan helstrom <orjan.helstrom@gmail.com>
2013-09-27 10:19:43 +0200

    Added Producer index and detail view. Added link from product to its producer.

--- FoodDay/urls.py
+++ FoodDay/urls.py
@@ -1,7 +1,8 @@
 from django import views
 from django.conf.urls import patterns, include, url
 
-from Products.views import ProductDetailView, ProductIndexView, ProductTypeDetailView, ProductTypeIndexView
+from Products.views import ProductDetailView, ProductIndexView, ProductTypeDetailView, ProductTypeIndexView, \
+    ProducerIndexView, ProducerDetailView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,6 +16,10 @@ urlpatterns = patterns('',
     # PRODUCT
     url(r'^product/$', ProductIndexView.as_view(), name='product_list'),
     url(r'^product/(?P<pk>\d+)/$', ProductDetailView.as_view(), name='product_detail'),
+
+    # PRODUCER
+    url(r'^producer/$', ProducerIndexView.as_view(), name='producer_list'),
+    url(r'^producer/(?P<pk>\d+)/$', ProducerDetailView.as_view(), name='producer_detail'),
 
     # PRODUCT_TYPE
     url(r'^productType/$', ProductTypeIndexView.as_view(), name='product_type_list'),
--- Products/views.py
+++ Products/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.views import generic
-from Products.models import Product, ProductType
+from Products.models import Product, ProductType, Producer
 
 
 class ProductIndexView(generic.ListView):
@@ -14,6 +14,19 @@ class ProductIndexView(generic.ListView):
 class ProductDetailView(generic.DetailView):
     model = Product
     template_name = "products/product_detail.html"
+
+
+class ProducerIndexView(generic.ListView):
+    template_name = "products/producer_index.html"
+    context_object_name = "producer_list"
+
+    def get_queryset(self):
+        return Producer.objects.order_by('name')
+
+
+class ProducerDetailView(generic.DetailView):
+    model = Producer
+    template_name = 'products/producer_detail.html'
 
 
 class ProductTypeIndexView(generic.ListView):
