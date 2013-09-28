b8d5f3bd8bf26e216f0673080b55f06489949579
Tobias G. Waaler <tobiasgwaaler@gmail.com>
2013-09-28 21:32:03 +0200

    Created a test for verifying the correctness of the price per mass calculation

--- Products/tests.py
+++ Products/tests.py
@@ -4,13 +4,22 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+from Products.models import Product, Producer
 
 from django.test import TestCase
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+    def setUp(self):
+        self.producer = Producer(name='Tine meierier')
+        self.producer.save()
+        self.product = Product(name='Jarlsberg med 27% fett',
+                               price=121.,
+                               mass=700.,
+                               massUnit=Product.GRAM,
+                               producer=self.producer)
+        self.product.save()
+
+    def test_pretty_price_per_mass(self):
+        self.assertEqual(self.product.pretty_price_per_mass(), u'172.86 kr/kg')
