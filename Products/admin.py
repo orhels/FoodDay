d3d0acfc56ea811cb70c64d4dced90279331fbec
orjan helstrom <orjan.helstrom@gmail.com>
2013-10-02 19:26:44 +0200

    Added a nicer product_category selection in admin. Still shows ALL categories :/

--- Products/admin.py
+++ Products/admin.py
@@ -6,6 +6,7 @@ from Products.models import Product, Producer, ProductCategory
 class ProductAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_display = ['name', 'price', 'producer', 'mass', 'massUnit', 'description', 'vegetarian', 'vegan']
+    filter_horizontal = ['productCategories']
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Producer)
