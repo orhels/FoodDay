f8de1ec9062a57abc449df790cb9cc9cd854ae19
orjan helstrom <orjan.helstrom@gmail.com>
2013-10-03 21:11:33 +0200

    Updated product admin to only show childlevel product_category.

--- Products/admin.py
+++ Products/admin.py
@@ -8,6 +8,13 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'producer', 'mass', 'massUnit', 'description', 'vegetarian', 'vegan']
     filter_horizontal = ['productCategories']
 
+    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+        if db_field.name == 'productCategories':
+            kwargs['queryset'] = ProductCategory.objects.filter(children=None)
+        return super(ProductAdmin,self).formfield_for_manytomany(db_field, request, **kwargs)
+
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Producer)
 admin.site.register(ProductCategory)
