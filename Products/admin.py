e6b6e7ce74d31bd3e6abd7369bbba472c95299b6
Tobias G. Waaler <tobiasgwaaler@gmail.com>
2013-10-03 23:07:21 +0200

    Created a more responsive layout

--- Products/admin.py
+++ Products/admin.py
@@ -3,6 +3,7 @@ __author__ = 'orjan'
 from django.contrib import admin
 from Products.models import Product, Producer, ProductCategory
 
+
 class ProductAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_display = ['name', 'price', 'producer', 'mass', 'massUnit', 'description', 'vegetarian', 'vegan']
@@ -11,8 +12,7 @@ class ProductAdmin(admin.ModelAdmin):
     def formfield_for_manytomany(self, db_field, request=None, **kwargs):
         if db_field.name == 'productCategories':
             kwargs['queryset'] = ProductCategory.objects.filter(children=None)
-        return super(ProductAdmin,self).formfield_for_manytomany(db_field, request, **kwargs)
-
+        return super(ProductAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
 
 
 admin.site.register(Product, ProductAdmin)
