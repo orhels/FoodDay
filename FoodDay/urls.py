34ef6c93dfc21a8d0a80b41f1753015a961d51f8
orjan helstrom <orjan.helstrom@gmail.com>
2013-10-08 21:22:16 +0200

    Product category is now loaded by AJAX. The url in the browser window is not updated. Added a onclick event to the whole panel-heading in sidebar. It now opens/closes anywhere you click.

--- FoodDay/urls.py
+++ FoodDay/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 from Products.views import ProductDetailView, ProductIndexView, ProductCategoryDetailView, ProductCategoryIndexView, \
-    ProducerIndexView, ProducerDetailView
+    ProducerIndexView, ProducerDetailView, product_category
 from Misc.views import FrontPageView
 
 # Uncomment the next two lines to enable the admin:
@@ -33,7 +33,8 @@ urlpatterns = patterns('',
 
     # PRODUCT_CATEGORY
     url(r'^product-category/$', ProductCategoryIndexView.as_view(), name='product_category_list'),
-    url(r'^product-category/(?P<pk>\d+)/$', ProductCategoryDetailView.as_view(), name='product_category_detail'),
+    url(r'^product-category/(?P<pk>\d+)/$', product_category, name='product_category_detail'),
+    #url(r'^product-category/(?P<pk>\d+)/$', ProductCategoryDetailView.as_view(), name='product_category_detail'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- static/custom/custom/sidebar.js
+++ static/custom/custom/sidebar.js
@@ -5,17 +5,27 @@ var Sidebar = {
     },
 
     makeSidebarClickable: function(){
-        var menuItems = $('div#sidebar').find('div.panel-heading');
-        $.each(menuItems, this.addMenuItemListener);
+        var menuHeader = $('div#sidebar').find('div.panel-heading');
+        $.each(menuHeader, this.addMenuHeaderListener);
+        var menuSubcat = $('div#sidebar').find('div.sidebar-subcat');
+        $.each(menuSubcat, this.addMenuSubcatListener);
     },
 
-    addMenuItemListener: function(index, element){
-        $(element).on('click', this.menuItemEventHandler);
+    addMenuHeaderListener: function(index, element){
+        //console.log(element);
+        $(element).on('click', function(){
+            //console.log($(element).find('a.sidebar-toggle')[0])
+            $(element).find('a.sidebar-toggle')[0].click();
+        });
     },
 
-    menuItemEventHandler: function(event){
-        console.log(event.targetElement);
+
+    addMenuSubcatListener: function(index, element){
+        $(element).on("click", function() {
+            $.get("/product-category/"+ $(element).data('product_cat_id'), function(data){
+                $('#main-content').html(data);
+            });
+        });
     }
-
 
 };
