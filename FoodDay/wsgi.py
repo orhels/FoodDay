c7548e480e73df2e4dc53082573952b1a4bfc73f
Tobias G. Waaler <tobiasgwaaler@gmail.com>
2013-10-11 17:18:58 +0000

    Add gunicorn config file and modify wsgi.py to work with gunicorn

--- FoodDay/wsgi.py
+++ FoodDay/wsgi.py
@@ -18,10 +18,8 @@ import os
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "FoodDay.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FoodDay.Settings.production_settings")
+# os.environ["DJANGO_SETTINGS_MODULE"] = "FoodDay.Settings.local_settings"
+#print os.environ["DJANGO_SETTINGS_MODULE"]
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
