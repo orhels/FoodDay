7f72e530ffac53428c1f57f25b2e351cb86ee70e
Tobias G. Waaler <tobiasgwaaler@gmail.com>
2013-10-12 00:12:35 +0200

    Created initial production and test settings

--- FoodDay/Settings/production_settings.py
+++ FoodDay/Settings/production_settings.py
@@ -4,10 +4,4 @@ from base_settings import *
 I_AM_ONLY_DOING_THIS_TO_TRICK_PYCHARMS
 
 
-import dj_database_url
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
---
+++ FoodDay/Settings/test_settings.py
@@ -0,0 +1,16 @@
+# coding=UTF-8
+from base_settings import *
+
+I_AM_ONLY_DOING_THIS_TO_TRICK_PYCHARMS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'foodaytest',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'foodaytest',
+        'PASSWORD': 'c*rw1ÞY8w$HÞæwÝ',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
