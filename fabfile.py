b60ac9f4e8da98d49cc46b69fa9c536f0de94790
Tobias G. Waaler <tobiasgwaaler@gmail.com>
2013-10-13 13:39:23 +0200

    Updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,10 @@
 # coding=UTF-8
-import os
 import sys
 
-from fabric.operations import local
+from fabric.context_managers import cd, shell_env
+from fabric.contrib.console import confirm
+from fabric.decorators import hosts
+from fabric.operations import local, run
 
 LOCAL_SETTINGS = "FoodDay.Settings.local_settings"
 
@@ -12,43 +14,34 @@ _old_environment_value = ""
 def deploy_to_test():
     print "Deploying to test"
     print ""
-    try:
-        _set_environment_variables()
+    with shell_env(DJANGO_SETTINGS_MODULE=LOCAL_SETTINGS):
         _print_debug_info()
         _run_tests()
         _commit_to_test_branch()
         _push_to_remote()
         _update_on_server()
         _restart_server()
-    finally:
-        _reset_environment_variables()
 
 
 def _commit_to_test_branch():
+    confirm("Your local changes will be commited to the test branch. Ok?")
     local('git checkout test')
     local('git commit -a')
 
-def _set_environment_variables():
-    _old_environment_value = os.getenv('DJANGO_SETTINGS_MODULE', "")
-    os.putenv('DJANGO_SETTINGS_MODULE', LOCAL_SETTINGS)
-
-
-def _reset_environment_variables():
-    if _old_environment_value == "":
-        os.unsetenv('DJANGO_SETTINGS_MODULE')
-    else:
-        os.putenv('DJANGO_SETTINGS_MODULE', _old_environment_value)
-
 
 def _restart_server():
-    print "TODO: sudo initctl restart foodaytest_gunicorn"
+    print "TODO: emit restart signal"
 
 
 def _push_to_remote():
     local('git push origin test')
 
+
+@hosts('foodaytest@fooday.no')
 def _update_on_server():
-    print "TODO: 'git pull origin test' as foodaytest@fooday.no"
+    code_dir = '/home/foodaytest/app/FoodDay'
+    with cd(code_dir):
+        run('git pull origin test')
 
 
 def _virtualenv_has_been_activated():
