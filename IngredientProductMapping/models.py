224e011e069e218baf25198d6276358cb2ba77f4
Tobias G. Waaler <tobiasgwaaler@gmail.com>
2013-10-20 11:45:11 +0200

    Created a method where our solution to the unbounded knapsack problem can be implemented

--- IngredientProductMapping/models.py
+++ IngredientProductMapping/models.py
@@ -9,16 +9,56 @@ MILLILITER = 'ml'
 UNITS = ((KILOGRAM, 'kilogram'), (GRAM, 'gram'), (LITER, 'liter'), (DESILITER, 'desiliter'), (MILLILITER, 'milliliter'))
 
 
+def choose_cheapest_product(ingredient_quantity, product_list):
+    #product_list = [(price per unit, id, price, quantity)]
+
+    #Setup
+    cur_list_index = 0
+    reached_quantity = 0
+    quantity_left = ingredient_quantity
+    QUANTITY = 3
+    PRICE = 2
+    ID = 1
+    result = {}
+
+    # 1: take as many as you can of the cheapest (lowest unit price)
+    for product in product_list:
+        #setup
+        product_quantity = product[QUANTITY]
+        product_id = product[ID]
+        #Add to cart if quantity is lower than remaining quantity
+        if product_quantity <= quantity_left:
+            number_of_products = quantity_left / product_quantity
+            quantity_left -= number_of_products * product_quantity
+            result.update({product_id: number_of_products})
+            break
+
+
+    # 2: try all permutations to fill up the remaining
+    best_price = 999999999
+    best_product = None
+    for product in product_list:
+        if product[QUANTITY] >= quantity_left:
+            if product[PRICE] < best_price:
+                best_price = product[PRICE]
+                best_product = product[ID]
+    result.update({best_product: 1})
+    return result
+
+
 class IngredientProductMapping(models.Model):
     #Fields
     name = models.CharField(max_length=150)
 
     def get_products_for_cart(self, total_ingredient_quantity, ingredient_quantity_type):
         ingredient_quantity = ingredient_quantity_type.get_quantity_multiplier_converter() * total_ingredient_quantity
+        product_list = []
         for product in self.products:
-            product.quantityType.get_quantity_multiplier_converter() * product.quantity
-            # todo
-            [('product_id', 'pris', 'vekt'), (),()]
+            product_quantity = product.quantityType.get_quantity_multiplier_converter() * product.quantity
+            product_list.append((product.price/product_quantity, product.id, product.price, product_quantity))
+        product_list.sort()
+        return choose_cheapest_product(ingredient_quantity, product_list)
+
 
 
 
--- Recipes/tests.py
+++ Recipes/tests.py
@@ -1,62 +1,93 @@
 # coding=UTF-8
 from django.test import TestCase
-from IngredientProductMapping.models import QuantityType, GRAM, IngredientProductMapping
-from Products.models import Product, Producer
-
-from Recipes.models import Recipe, Ingredient
+from IngredientProductMapping.models import choose_cheapest_product
 
 
 class TestRecipe(TestCase):
-    def setUp(self):
-        self.carbonaraRecipe = Recipe(
-            name="Carbonara",
-            description="",
-            procedure="",
-            servings=4,
-            processing_time="",
-            image="",
-        )
-        self.carbonaraRecipe.save()
+    #def setUp(self):
+    #    self.carbonaraRecipe = Recipe(
+    #        name="Carbonara",
+    #        description="",
+    #        procedure="",
+    #        servings=4,
+    #        processing_time="",
+    #        image="",
+    #    )
+    #    self.carbonaraRecipe.save()
+    #
+    #    self.gramQT = QuantityType(
+    #        name=GRAM,
+    #        category='vekt'
+    #    )
+    #    self.gramQT.save()
+    #
+    #    self.producer = Producer(
+    #        name="Toro"
+    #    )
+    #    self.producer.save()
+    #
+    #    self.pastaProduct = Product(
+    #        name="pasta",
+    #        description="",
+    #        price=35,
+    #        quantity=120,
+    #        image="",
+    #        quantityType=self.gramQT,
+    #        producer=self.producer,
+    #    )
+    #    self.pastaProduct.save()
+    #
+    #    self.pastaIngredient = Ingredient(
+    #        name="pasta",
+    #        quantity=550,
+    #        recipe=self.carbonaraRecipe,
+    #        quantityType=self.gramQT
+    #    )
+    #    self.pastaIngredient.save()
+    #
+    #    self.pastaIngredientMapping = IngredientProductMapping(
+    #        name="pasta")
+    #    self.pastaIngredientMapping.save()
+    #    self.pastaIngredientMapping.ingredient.add(self.pastaIngredient)
+    #    self.pastaIngredientMapping.product.add(self.pastaProduct)
+    #    self.pastaIngredientMapping.save()
+    #
+    #
+    #def test_get_products_for_cart(self):
+    #    self.assertEqual(
+    #        self.carbonaraRecipe.get_products_for_cart(3),
+    #        {'1': 8})
 
-        self.gramQT = QuantityType(
-            name=GRAM,
-            category='vekt'
-        )
-        self.gramQT.save()
-
-        self.producer = Producer(
-            name="Toro"
-        )
-        self.producer.save()
-
-        self.pastaProduct = Product(
-            name="pasta",
-            description="",
-            price=35,
-            quantity=120,
-            image="",
-            quantityType=self.gramQT,
-            producer=self.producer,
-        )
-        self.pastaProduct.save()
-
-        self.pastaIngredient = Ingredient(
-            name="pasta",
-            quantity=550,
-            recipe=self.carbonaraRecipe,
-            quantityType=self.gramQT
-        )
-        self.pastaIngredient.save()
-
-        self.pastaIngredientMapping = IngredientProductMapping(
-            name="pasta")
-        self.pastaIngredientMapping.save()
-        self.pastaIngredientMapping.ingredient.add(self.pastaIngredient)
-        self.pastaIngredientMapping.product.add(self.pastaProduct)
-        self.pastaIngredientMapping.save()
-
-
-    def test_get_products_for_cart(self):
+    def test_choose_cheapest_product(self):
         self.assertEqual(
-            self.carbonaraRecipe.get_products_for_cart(3),
-            {'1': 8})
+            choose_cheapest_product(800,
+                        [
+                           #(price per unit, id, price, quantity)
+                            (0.1,    'stor pakke',         120, 1200),
+                            (0.17,   'mellomstor pakke',   70,  400),
+                            (0.25,   'liten pakke',        50,  200)
+                        ]),
+            {'stor pakke': 1}
+        )
+        self.assertEqual(
+            choose_cheapest_product(100,
+                        [
+                           #(price per unit, id, price, quantity)
+                            (0.1,    'stor pakke',         120, 1200),
+                            (0.17,   'mellomstor pakke',   70,  400),
+                            (0.25,   'liten pakke',        50,  200)
+                        ]),
+            {'liten pakke': 1}
+        )
+
+    def test_choose_cheapest_product_hard(self):
+        self.assertEqual(
+            choose_cheapest_product(600,
+                                    [
+                                        (0.300, 'stor pakke',       350, 1200),
+                                        (0.375, 'mellomstor pakke', 150, 400),
+                                        (0.450, 'liten pakke',      90,  200)
+                                    ]),
+            {'liten pakke': 1, 'mellomstor pakke': 1}
+        )
+
