5e37a12fecc0f954f58b7ba2e77d1b64ed477380
Tobias G. Waaler <tobiasgwaaler@gmail.com>
2013-10-26 00:03:24 +0200

    Switched to Postgres locally. To start using this you have to a running instance of Postgres on your local machine and create a database as described in the README

--- FoodDay/Settings/local_settings.py
+++ FoodDay/Settings/local_settings.py
@@ -8,10 +8,10 @@ TEMPLATE_DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'foodday_database_file',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'fooday_local',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
+        'USER': 'fooday_local',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
--- README.md
+++ README.md
@@ -3,11 +3,15 @@ FoodDay
 
 - Clone this repo
 - Install virtualenv, initialize a new virtual environment for this app and install all dependencies into this environment by running ```pip install -r requirements.txt```
-- Install bootstrap by running
-```
-git submodule init 
-git submodule update
-```
 
-- ```python manage.py syncdb```
+You will need to have an instance of Postgres running on your machine, with a database user called fooday_local and a database with the same name:
+```createuser fooday_local```
+```createdb fooday_local -O fooday_local```
+
+
+```python manage.py syncdb```
+
+Run the server locally like this:
 - ```python manage.py runserver```
+
+
